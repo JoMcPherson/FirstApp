5c7554a5a70d69c3573042689aeef36b93362ab3
McPherson <joanna.mcpherson@cvent.com>
2024-06-25 11:49:20 -0400

    added payroll tax calc

--- src/main/java/org/example/PayrollCalculator.java
+++ src/main/java/org/example/PayrollCalculator.java
@@ -10,6 +10,8 @@ public class PayrollCalculator {
      float hours = scanner.nextInt();
      System.out.println("Enter their hourly pay:");
      int pay = scanner.nextInt();
+     System.out.println("Enter their filing status (1=Single, 2=Married, 3=Head of Household):");
+     int filing = scanner.nextInt();
      float overtime = 0;
      String note = "";
      if (hours > 40) {
@@ -17,8 +19,51 @@ public class PayrollCalculator {
          hours = 40;
      note = "("  +name + " earned overtime)";}
      double gross = hours*pay + overtime*pay*1.5;
-     System.out.println(String.format("%s has $%.2f gross pay %s",name, gross, note));
+    double fed = 0;
+     if (gross <= 800) {
+         switch (filing) {
+             case (1):
+                 fed = 0.07;
+                 break;
+             case (2):
+                 fed = 0.06;
+                 break;
+             case (3):
+                 fed = 0.04;
+                 break;
+         }
+     }
+         else if(gross >= 800.01 && gross <=1600) {
+             switch(filing) {
+                 case 1:
+                 case 2:
+                     fed = 0.1;
+                     break;
+                 case 3:
+                     fed = 0.08;
+                     break;
+             }
+         }
+     else  {
+         switch(filing) {
+             case (1):
+                 fed = 0.15;break;
+             case (2):
+                 fed = 0.13;break;
+             case (3):
+                 fed = 0.11;break;
+         }
+     }
 
+   double social = 0.062*gross;
+     double medicare = 0.0145*gross;
+     double federal = fed*gross;
+     double net = gross - social - medicare - federal;
+     System.out.println(String.format("%s has $%.2f gross pay %s",name, gross, note));
+        System.out.println(String.format("social security tax: $%.2f", social));
+        System.out.println(String.format("medicare security tax: $%.2f", medicare));
+        System.out.println(String.format("federal security tax: $%.2f", federal));
+        System.out.println(String.format("net pay: $%.2f", net));
 
     }
 }

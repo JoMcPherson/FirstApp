79bc819cdc1e2c9892fb42e90fcd166bd3723488
McPherson <joanna.mcpherson@cvent.com>
2024-06-25 17:46:25 -0400

    sandwich loaded

--- src/main/java/org/example/SandwichShop.java
+++ src/main/java/org/example/SandwichShop.java
@@ -6,9 +6,20 @@ public class SandwichShop {
             Scanner scanner = new Scanner(System.in);
             System.out.println("What size sandwich you want (1-Regular, 2-Large):");
             int size = scanner.nextInt();
-            System.out.println("Enter your age to determine if you are eligible for a discount. Enter 0 if you\n" +
+            scanner.nextLine();
+        System.out.println("Do you want the sandwich loaded (double everything) (yes or no):");
+        String loaded = scanner.nextLine();
+        double additional = 0;
+        if (loaded.equals("yes") && size == 1)
+            additional = 1.00;
+        else if (loaded.equals("yes") && size ==2)
+            additional = 1.75;
+
+
+        System.out.println("Enter your age to determine if you are eligible for a discount. Enter 0 if you\n" +
                     "do not want an age-based discount:");
             int age = scanner.nextInt();
+
            double discount;
            double price = 0.0;
             if (age <= 17)
@@ -28,7 +39,7 @@ public class SandwichShop {
                     System.out.println("not a size");
             }
 
-            price = price*discount;
+            price = (price+additional)*discount;
             double sales = price*.0825;
         System.out.printf("Sandwich Price: $%.2f%n", price);
         System.out.printf("Sales tax: $%.2f%n", sales);
